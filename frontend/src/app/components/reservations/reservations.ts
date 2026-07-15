c03cec958cd6f610881fc2d4156cfefa62b4c9be
MalekDev420 <malek.benkacem@esprit.tn>
2026-07-15 22:32:44 +0100

    avancement front

--- frontend/src/app/components/reservations/reservations.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-reservations',
-  imports: [],
-  templateUrl: './reservations.html',
-  styleUrl: './reservations.css',
-})
-export class Reservations {}
